e76c0f69d17f938adf9e0c73ccbc293aea21cbfd
jackpender <20085278@mail.wit.ie>
2019-04-03 15:06:18 +0100

    DavidsPythonBeGone

--- Chapter2ex,David/Q3.py
+++
@@ -1,5 +0,0 @@
-Q3.
-1.79769×10+308
-
-
-#The largest possible floating point value available on this system. 
--- Chapter2ex,David/Q6.py
+++
@@ -1,2 +0,0 @@
-Q6.
-#Must declare x = 10 and not 10 = x as you can not assign to literal
