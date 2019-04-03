de962c131d75c73d436b51d613189b61fc7d989f
ginge2000 <dwdavidwhite21@gmail.com>
2019-04-03 14:56:02 +0100

    Paul

--- Chapter2ex, Jack/Q16/16.py
+++
@@ -1,3 +0,0 @@
-print("n")
-print("\n") #"\n" breaks up the line, without the backslash you are just printing the letter n
-print("n")
--- Chapter2ex, Jack/Q4/Q4.py
+++
@@ -1,4 +0,0 @@
-print(-0.0001)
-
-# This is the smallest number that can be represented
-# by proper numbers and not scientific notation
